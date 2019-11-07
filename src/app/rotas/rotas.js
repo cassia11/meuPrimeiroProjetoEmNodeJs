2b466bc3a50e5ca504d773e3f3ac79d260158a01
cassia11 <cassiagomes0102@gmail.com>
2019-11-07 01:51:50 -0300

    torna template lista.marko dinamico

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -17,7 +17,20 @@ app.get('/', function(req, resp) {
 
 app.get('/livros', function(req, resp) {
     resp.marko(
-       require('../views/livros/lista/lista.marko')
+       require('../views/livros/lista/lista.marko'),
+       {
+        
+        livros: [
+            { 
+                id: 1,
+                titulo: 'Fundamentos do Node'
+            },
+            { 
+                id: 2,
+                titulo: 'Node Avançado'
+            }
+        ]
+       }
     );
 });
 };
