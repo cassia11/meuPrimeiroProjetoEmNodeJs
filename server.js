a3785262feab93602ec3c123051a7d8b0ce5262e
cassia11 <cassiagomes0102@gmail.com>
2019-11-06 23:51:34 -0300

    começando a user express

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor  = http.createServer(function (req, resp) {
-    
-    let html = '';
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-    `;
-    } else if (req.url == '/livros')
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Listagem de livros </h1>
-            </body> 
-        </html>
-    `;
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
 
-    resp.end(html);
 });
-servidor.listen(3000);
+
+// const http = require('http');
+
+// const servidor  = http.createServer(function (req, resp) {
+    
+//     let html = '';
+//     if (req.url == '/') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Casa do Código </h1>
+//                 </body> 
+//             </html>
+//     `;
+//     } else if (req.url == '/livros')
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Listagem de livros </h1>
+//             </body> 
+//         </html>
+//     `;
+
+//     resp.end(html);
+// });
+// servidor.listen(3000);
